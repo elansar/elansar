3ec00d710d88ba0b6582299cfbcf7c864cada187
elansar <h.kaplan@baskentsistem.com>
2017-07-24 15:57:13 +0300

    Add files via upload

---
+++ bolge.php
@@ -0,0 +1,14 @@
+
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+</head>
+
+<?php  include 'wp-content/themes/politicize/header.php';
+?>
+
+<body>
+    <?php include 'subeler.html'; ?>
+    </body>
+</html>
+  
